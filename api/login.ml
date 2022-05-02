71aa3e3ac0043838a9c65f2907368a1c30e05be3
DaiF <julie.fiadino@gmail.com>
2022-05-02 17:45:39 +0200

    [ADD] http requests global functions

--- api/login.ml
+++ api/login.ml
@@ -2,10 +2,8 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
-let url = "https://roboats.virtualregatta.com/api"
-
-let body =
-    Client.get (Uri.of_string (url + "/login")) >>= fun (resp, body) ->
+let httpget url =
+    Client.get (Uri.of_string (url)) >>= fun (resp, body) ->
         let code = resp |> Response.status |> Code.code_of_status in
         Printf.printf "Response code: %d\n" code;
         Printf.printf "Headers: %s\n" (resp |> Response.headers |>
@@ -13,8 +11,20 @@ let body =
         body |> Cohttp_lwt.Body.to_string >|= fun body ->
             Printf.printf "Body of length: %d\n" (String.length
         body);
-    body
+    body;;
+
+let httppost url content =
+    Client.post (Uri.of_string (url)) >>= fun (resp, body) ->
+        let code = resp |> Response.status |> Code.code_of_status in
+        Printf.printf "Response code: %d\n" code;
+        Printf.printf "Headers: %s\n" (resp |> Response.headers |>
+        Header.to_string);
+        body |> Cohttp_lwt.Body.to_string >|= fun body ->
+            Printf.printf "Body of length: %d\n" (String.length
+        body);
+    body;;
+
 
 let () =
-    let body = Lwt_main.run body in
+    let body = Lwt_main.run (httpget "https://static.virtualregatta.com/winds/live/references.json") in
     print_endline ("Received body\n" ^ body)
