f3ac09bd2637bf79223f73638a22c42f1af3a3e7
paul Dufour <paul.dufour@epita.fr>
2022-05-03 16:16:49 +0000

    [ADD] post

--- api/login.ml
+++ api/login.ml
@@ -2,7 +2,7 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
-let httpget url =
+let getData url =
     Client.get (Uri.of_string (url)) >>= fun (resp, body) ->
         let code = resp |> Response.status |> Code.code_of_status in
         Printf.printf "Response code: %d\n" code;
@@ -13,18 +13,43 @@ let httpget url =
         body);
     body;;
 
-let httppost url content =
-    Client.post (Uri.of_string (url)) >>= fun (resp, body) ->
-        let code = resp |> Response.status |> Code.code_of_status in
+
+let reqBody url content = 
+  let uri = Uri.of_string url in
+  let headers = Header.init ()
+    |> fun h -> Header.add h "Content-Type" "text/plain"
+  in
+  let body = Cohttp_lwt.Body.of_string content in
+  Client.call ~headers ~body `POST uri >>= fun (resp, body) ->
+   let code = resp |> Response.status |> Code.code_of_status in
         Printf.printf "Response code: %d\n" code;
         Printf.printf "Headers: %s\n" (resp |> Response.headers |>
         Header.to_string);
         body |> Cohttp_lwt.Body.to_string >|= fun body ->
             Printf.printf "Body of length: %d\n" (String.length
         body);
-    body;;
+    body;; 
+
+
+let load name =
+    let ic = open_in name in
+    let try_read () =
+        try Some ( input_line ic ) with End_of_file -> None in
+    let rec loop () = match try_read () with
+        | Some s -> s
+        | None -> ""
+    in loop () ;;
 
 
 let () =
-    let body = Lwt_main.run (httpget "https://static.virtualregatta.com/winds/live/references.json") in
-    print_endline ("Received body\n" ^ body)
+  let respBody = Lwt_main.run (reqBody
+    "https://roboats.virtualregatta.com/api/login"
+    (load "api/login.config")) in
+  print_endline (respBody)
+
+(*let () =
+    let body = Lwt_main.run (httppost
+    "https://roboats.virtualregatta.com/api/login"
+    param
+) in
+        print_endline ("Received body\n" ^ body)*)
