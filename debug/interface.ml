ed836c634dec8d5dd476ea7df2dddd1859486a6a
DaiF <julie.fiadino@gmail.com>
2022-06-15 18:09:06 +0200

    Switch debug compilation to dune

--- debug/interface.ml
+++ debug/interface.ml
@@ -12,5 +12,3 @@ let rec window () =
     let event = wait_next_event [Key_pressed] in
     if event.key == 'q' then exit 0
     else print_char event.key; print_newline (); window ();;
-
-window();;
